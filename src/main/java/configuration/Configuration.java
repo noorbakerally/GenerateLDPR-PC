d0a05db76840e364630e8bb14816cf3ceaca9eee
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-05-29 11:32:20 +0200

    sample configuration added

--- src/main/java/configuration/Configuration.java
+++ src/main/java/configuration/Configuration.java
@@ -1,8 +1,11 @@
 package configuration;
 
+import java.util.List;
+
 /**
  * Created by bakerally on 5/29/17.
  */
 public class Configuration {
     String baseURI;
+    List<Container> containers;
 }
