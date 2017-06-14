a6390aadff1776d29e851b3493b82451f99b09c6
Noorani Bakerally <noorani.bakerally@gmail.com>
2017-06-14 17:35:40 +0200

    sending request

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,19 +1,45 @@
-import fr.emse.opensensingcity.configuration.Configuration;
-import fr.emse.opensensingcity.configuration.ConfigurationFactory;
+
+import org.apache.http.Header;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.jena.rdf.model.Model;
-import org.apache.jena.riot.RDFDataMgr;;import java.io.File;
+import org.apache.jena.riot.RDFDataMgr;
+import org.restlet.Client;
+import org.restlet.Request;
+import org.restlet.Response;
+import org.restlet.data.Method;
+import org.restlet.data.Protocol;;import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 /**
  * Created by bakerally on 5/29/17.
  */
 public class main {
-    public static void main(String args []){
+    public static void main(String args []) throws Exception {
         ClassLoader classLoader = main.class.getClassLoader();
         File file = new File(classLoader.getResource("Configuration.ttl").getFile());
 
         //ConfigurationFactory.createConfiguration("main/resources/Configuration.ttl");
-        Configuration configuration = ConfigurationFactory.createConfiguration(file.getAbsolutePath());
+        //Configuration configuration = ConfigurationFactory.createConfiguration(file.getAbsolutePath());
 
+
+        HttpClient client = HttpClientBuilder.create().build();
+        String content = "<> a <http://example.com/ParkingFacility>";
+        HttpPost httpPost = new HttpPost("http://localhost:8081/rest/");
+        httpPost.addHeader("Content-Type","text/turtle");
+        httpPost.addHeader("Slug","noorparking17");
+        httpPost.setEntity(new StringEntity(content));
+        HttpResponse response = client.execute(httpPost);
+
+        System.out.println(response.getStatusLine());
 
 
 
